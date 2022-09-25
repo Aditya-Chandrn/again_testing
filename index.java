19a9ecc122c586ee07c2c0e3841a14c90d88618d
Aditya-Chandrn <adityachandrn1@gmail.com>
2022-09-25 19:37:23 +0530

    changed int to string

--- index.java
+++ index.java
@@ -5,7 +5,7 @@ class index{
         System.out.print("\nhello guys.");
         Scanner input = new Scanner(System.in);
         System.out.print("\nKoi bhi number dal do : ");
-        int num=input.nextInt();
+        String num=input.nextLine();
         System.out.print("\n    Hamara number hai : "+num);
 
         input.close();
