a3b7c22448f3f6fce36efb4977923c77e66ef435
Aditya-Chandrn <adityachandrn1@gmail.com>
2022-09-25 19:34:14 +0530

    displays output

--- index.java
+++ index.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 class index{
     public static void main(String[] args) {
-        System.out.println("hello guys.");
+        System.out.print("\nhello guys.");
         Scanner input = new Scanner(System.in);
-        System.out.println("Koi bhi number dal do : ");
+        System.out.print("\nKoi bhi number dal do : ");
         int num=input.nextInt();
+        System.out.print("\n    Hamara number hai : "+num);
+
         input.close();
     }
 }
