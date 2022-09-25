0845eed29e0fe9405ee2a1e0ee117e7c544b9ab8
Aditya-Chandrn <adityachandrn1@gmail.com>
2022-09-25 19:26:11 +0530

    added input code

--- index.java
+++ index.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 class index{
     public static void main(String[] args) {
         System.out.println("hello guys.");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Koi bhi number dal do : ");
+        int num=input.nextInt();
+        input.close();
     }
 }
